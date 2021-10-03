17c409a1e2c448d1881a622d9bd507c6e06d9fd2
laukuansin <49442955+laukuansin@users.noreply.github.com>
2021-10-03 14:11:33 +0800

    Update Mission.js

--- models/Mission.js
+++ models/Mission.js
@@ -4,6 +4,8 @@ const Log = require("./Log").schema;
 const MissionSchema = new mongoose.Schema(
   {
     logs: [Log],
+    roomOwner: {type: String},
+    roomName:{type:String}
   },
   {
     timestamps: true,
